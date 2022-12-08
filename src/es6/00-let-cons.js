fd6a40500e0247deddcee79c40aff715383d5f44
esteban cespedes monestel <mmonestelalf@gmail.com>
2022-12-08 09:21:12 -0600

    Course of js em6

---
+++ src/es6/00-let-cons.js
@@ -0,0 +1,18 @@
+/*var lastName = 'David';
+lastName = 'Varela';
+console.log(lastName);*/
+
+
+const fruits = () =>{
+if(true){
+var fruit1 = 'Apple';
+let fruit2 = 'Kiwi';
+const fruit3 = 'Banana';
+}  
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
+}
+
+
+fruits();
---
+++ src/es6/03-strings.js
@@ -0,0 +1,9 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' '+ world + '!';
+console.log(epicPhrase);
+
+
+//Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
