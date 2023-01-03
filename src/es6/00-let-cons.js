6a89ffab41c15756955a23fbd4f305cfc519c179
esteban cespedes monestel <mmonestelalf@gmail.com>
2023-01-03 12:19:47 -0600

    string literals

---
+++ src/es6/00-let-cons.js
@@ -0,0 +1,21 @@
+var lastName = 'David';
+lastName = 'Oscar';
+console.log(lastName);
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'Cat';
+console.log(animal);
+
+
+const fruits = () => {
+    if(true){
+        var fruit1 = 'Apple';
+        let fruit2 = 'Kiwi';
+        const fruit3 = 'Banana';
+    }
+  console.log(fruit);
+}
---
+++ src/es6/03-strings.js
@@ -0,0 +1,9 @@
+var hello = 'Hello';
+var world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+
+
+// Template literals
+var epicPhrase2 = `${hello} string text`;
+
+console.log(epicPhrase2);
