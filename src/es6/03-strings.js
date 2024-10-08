b9330a842f6b607bc5f6b51841ed3750e35ec976
teban192 <mmonestelalf@gmail.com>
2024-10-08 12:20:36 -0600

    java script refresh course

--- src/es6/03-strings.js
+++
@@ -1,9 +0,0 @@
-var hello = 'Hello';
-var world = 'World';
-let epicPhrase = hello + ' ' + world + '!';
-
-
-// Template literals
-var epicPhrase2 = `${hello} string text`;
-
-console.log(epicPhrase2);
